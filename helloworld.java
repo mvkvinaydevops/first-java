c6d18aac556e43ed4f774bb0519c68693ab27991
vinaydevopsv1 <devops.tel.v1@gmail.com>
2023-06-18 14:43:46 +0000

    comite in linux

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,7 @@
 class HellowWorld {
     public static void main(String[] args) {
         System.out.println("hellow world");
+	System.out.println("hellow Git");
+	System.out.println("welcome to gitlinux");
     }
 }
